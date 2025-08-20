94a0cf4f5ec28c1f79977b6763fde2f342049c92
skrambol <skrambolito@gmail.com>
2025-08-20 08:26:29 +0800

    initial commit

---
+++ src/main/java/com/skrambol/vetter_clinic/VetterClinicApplication.java
@@ -0,0 +1,13 @@
+package com.skrambol.vetter_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetterClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetterClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skrambol/vetter_clinic/VetterClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrambol.vetter_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetterClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
