18b98b4302600f244326a026aa2df13a06dab6ed
skrambol <skrambolito@gmail.com>
2025-08-27 11:07:08 +0800

    feat/register: password encryption ONLY

--- src/main/java/com/skrambol/vetter_clinic/security/Config.java
+++ src/main/java/com/skrambol/vetter_clinic/security/Config.java
@@ -1,20 +1,13 @@
 package com.skrambol.vetter_clinic.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -32,23 +25,6 @@ public class Config {
 		http.csrf(csrf -> csrf.disable());
 
 		return http.build();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
-			PasswordEncoder passwordEncoder) {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-		return new ProviderManager(authenticationProvider);
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService(DataSource dataSource) {
-		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-		jdbcUserDetailsManager.setUsersByUsernameQuery("select username, password, is_active from users where username=?");
-
-		return jdbcUserDetailsManager;
 	}
 
 	@Bean
